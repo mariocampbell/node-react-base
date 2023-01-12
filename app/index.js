7f687ccca1f5bb3cfcd9e6907d11b0f915014f94
Mario Campbell <hector.campbell@mercadolibre.com>
2023-01-12 19:31:20 -0300

    app files

---
+++ app/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import 'core-js';
+import 'regenerator-runtime';
+import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import './styles.scss';
+import { Home, About } from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: 'about',
+    element: <About />,
+  },
+]);
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
